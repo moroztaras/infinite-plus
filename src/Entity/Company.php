be2531bd82116d51c2017b35a6ed2f4488d7678c
tarasmoroz <taras.moroz@ekreative.com>
2023-07-24 16:26:27 +0200

    Added relations between Company and Employee Added relations between Employee and Project

--- src/Entity/Company.php
+++ src/Entity/Company.php
@@ -3,6 +3,8 @@
 namespace App\Entity;
 
 use App\Repository\CompanyRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: CompanyRepository::class)]
@@ -25,8 +27,12 @@ class Company
     #[ORM\Column(name: 'country', length: 256)]
     private string $country;
 
+    #[ORM\OneToMany(mappedBy: 'company', targetEntity: Employee::class, cascade: ['persist', 'remove'])]
+    private Collection $employees;
+
     public function __construct()
     {
+        $this->employees = new ArrayCollection();
         $this->createUuid();
         $this->setDateTime();
     }
@@ -68,6 +74,49 @@ class Company
     public function setCountry(string $country): self
     {
         $this->country = $country;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<Employee>
+     */
+    public function getEmployees(): Collection
+    {
+        return $this->employees;
+    }
+
+    /**
+     * @param Collection<Employee> $employees
+     *
+     * @return $this
+     */
+    public function setEmployees(Collection $employees): self
+    {
+        $this->employees = $employees;
+
+        return $this;
+    }
+
+    public function addEmployee(Employee $employee): self
+    {
+        if (!$this->employees->contains($employee)) {
+            $this->employees[] = $employee;
+            $employee->setCompany($this);
+        }
+
+        return $this;
+    }
+
+    public function removeEmployee(Employee $employee): self
+    {
+        if ($this->employees->contains($employee)) {
+            $this->employees->removeElement($employee);
+            // set the owning side to null (unless already changed)
+            if ($employee->getCompany() === $this) {
+                $employee->setCompany(null);
+            }
+        }
 
         return $this;
     }
