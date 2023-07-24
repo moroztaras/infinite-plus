b32f54108185813f1f81dee303f395abfed68e89
tarasmoroz <taras.moroz@ekreative.com>
2023-07-24 12:07:03 +0200

    Added api url for create new project

---
+++ src/Controller/Api/ProjectController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Controller\Api;
+
+use App\Entity\Employee;
+use App\Entity\Project;
+use App\Exception\Api\BadRequestJsonHttpException;
+use App\Manager\ProjectManager;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('api/project')]
+class ProjectController extends ApiController
+{
+    public function __construct(
+        private ProjectManager $projectManager
+    ) {
+    }
+
+    // Create new project
+    #[Route(path: '', name: 'api_project_create', methods: 'POST')]
+    public function create(Request $request): JsonResponse
+    {
+        /** @var Employee $employee */
+        $employee = $this->getEmployee($request);
+
+        if (!($content = $request->getContent())) {
+            throw new BadRequestJsonHttpException('Bad Request.');
+        }
+
+        return $this->json(['project' => $this->projectManager->createNewProject($content)], Response::HTTP_OK, [], ['create' => true]);
+    }
+}
---
+++ src/Normalizer/ProjectNormalizer.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Normalizer;
+
+use App\Entity\Project;
+use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
+
+class ProjectNormalizer implements NormalizerInterface
+{
+    /**
+     * @@param Project $object object to normalize
+     * @param null $format
+     *
+     * @return array|bool|float|int|string
+     */
+    public function normalize($object, $format = null, array $context = []): array
+    {
+        return [
+            'name' => $object->getName(),
+            'development_start_at' => $object->getDevelopmentStartAt()->format('d-m-Y'),
+            'createdAt' => $object->getCreatedAt()->format('d-m-Y'),
+        ];
+    }
+
+    public function denormalize($data, $class, $format = null, array $context = [])
+    {
+        if (isset($context['edit']) && isset($context['object_to_populate'])) {
+            $object = $context['object_to_populate'];
+
+            if (isset($data['name'])) {
+                $object->setName($data['name']);
+            }
+
+            return $object;
+        }
+    }
+
+    public function supportsNormalization($project, $format = null)
+    {
+        return $project instanceof Project;
+    }
+}
